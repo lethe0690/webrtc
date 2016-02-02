1610f7dc1454d92851355bc4f13a4a4ac896f397
lethe <lethe0690@gmail.com>
2016-02-02 17:53:55 -0500

    temp working version for one to many socket

--- js/socketTest.js
+++ js/socketTest.js
@@ -1,8 +1,338 @@
-/**
- * Created by lethe on 1/29/16.
- */
+'use strict';
+
+var socket = io.connect('https://localhost:2013');
+
+var peerconnectionList={}
+
+//peerconnectionList["Key1"] = new RTCPeerConnection(null);
+
+var extensionInstalled = false;
+var room;
+var roomOwner
+
+var isChannelReady;
+var isInitiator = false;
+var isStarted = false;
+var localStream;
+var remoteStream;
+
+var localVideo = document.querySelector('#videoContainer');
+var remoteVideo = document.querySelector('#videoContainer');
+
+var sdpConstraints = {'mandatory': {
+    'OfferToReceiveAudio':true,
+    'OfferToReceiveVideo':true }};
+
+//-------------socket listening--------------
+socket.on('created', function (room){
+    console.log('Created room ' + room);
+    isInitiator = true;
+});
+
+socket.on('full', function (room){
+    console.log('Room ' + room + ' is full');
+});
+
+socket.on('join', function (room,toSocketId){
+    console.log('Another peer made a request to join room ' + room);
+    console.log('This peer is the initiator of room ' + room + '!');
+    isChannelReady = true;
+    maybeStart(toSocketId);
+});
+
+socket.on('joined', function (room,roomOwnerSocketId){
+    console.log('This peer has joined room ' + room+"With Owner"+roomOwnerSocketId);
+    isChannelReady = true;
+    roomOwner = roomOwnerSocketId;
+});
+
+function sendMessage(message,toSocketId){
+    console.log('Client sending message: ', message);
+    // if (typeof message === 'object') {
+    //   message = JSON.stringify(message);
+    // }
+    socket.emit('message', message,toSocketId);
+}
 
 
-var socket = io.connect('https://192.168.1.88:2013');
+socket.on('message', function (message,toSocketId,fromSocketId){
+    console.log('Client received message:', message);
+    console.log('This message is from:',fromSocketId);
+    console.log('This message is to:',toSocketId);
 
-console.log(socket.manager);
+    //if(!isInitiator)
+    //{
+    //    toSocketId = roomOwner;
+    //    //console.log("----------------I should not be seen in broadcaster---------------");
+    //    //maybeStart(toSocketId);
+    //}
+
+    if (message === 'got user media')
+    {
+        maybeStart(toSocketId);
+    }
+    else if (message.type === 'offer') {
+        //console.log(toSocketId);
+
+        if (!isInitiator && !isStarted)
+        {
+            createPeerConnection(roomOwner);
+        }
+        //console.log(toSocketId);
+
+        peerconnectionList[roomOwner].setRemoteDescription(new RTCSessionDescription(message));
+        //console.log("I AM HERE ALREADY");
+
+        //console.log("Before do answer",toSocketId);
+        doAnswer(roomOwner);
+    }
+    else if (message.type === 'answer' && isStarted) {
+
+        //if(!peerconnectionList[toSocketId])
+        //{
+        //    maybeStart(toSocketId);
+        //}
+        console.log(toSocketId);
+
+        peerconnectionList[fromSocketId].setRemoteDescription(new RTCSessionDescription(message));
+    }
+    else if (message.type === 'candidate' && isStarted) {
+        var candidate = new RTCIceCandidate({
+            sdpMLineIndex: message.label,
+            candidate: message.candidate
+        });
+
+        //if(!peerconnectionList[toSocketId])
+        //{
+        //    maybeStart(toSocketId);
+        //}
+
+        peerconnectionList[fromSocketId].addIceCandidate(candidate);
+    }
+    else if (message === 'bye' && isStarted) {
+        handleRemoteHangup();
+    }
+});
+
+function maybeStart(toSocketId) {
+    //if (!isStarted && typeof localStream != 'undefined' && isChannelReady) {
+        if (typeof localStream != 'undefined' && isChannelReady) {
+
+        createPeerConnection(toSocketId);
+
+        if(isInitiator){
+            peerconnectionList[toSocketId].addStream(localStream);
+        }
+
+        isStarted = true;
+        console.log('isInitiator', isInitiator);
+        if (isInitiator) {
+            console.log("Before do call",toSocketId);
+            doCall(toSocketId);
+        }
+    }
+}
+
+function createPeerConnection(toSocketId) {
+    try {
+        peerconnectionList[toSocketId] = new RTCPeerConnection(null);
+
+        //pc = new RTCPeerConnection(null);
+        peerconnectionList[toSocketId].onicecandidate =
+
+            function(event)
+            {
+                console.log('handleIceCandidate event: ', event);
+                if (event.candidate) {
+                    sendMessage({
+                        type: 'candidate',
+                        label: event.candidate.sdpMLineIndex,
+                        id: event.candidate.sdpMid,
+                        candidate: event.candidate.candidate},toSocketId);
+                } else {
+                    console.log('End of candidates.');
+                }
+            }
+
+            //handleIceCandidate(toSocketId);
+        peerconnectionList[toSocketId].onaddstream = handleRemoteStreamAdded;
+        peerconnectionList[toSocketId].onremovestream = handleRemoteStreamRemoved;
+        console.log('Created RTCPeerConnnection');
+    } catch (e) {
+        console.log('Failed to create PeerConnection, exception: ' + e.message);
+        alert('Cannot create RTCPeerConnection object.');
+        return;
+    }
+}
+
+//function handleIceCandidate(event,toSocketId) {
+//    console.log('handleIceCandidate event: ', event);
+//    if (event.candidate) {
+//        sendMessage({
+//            type: 'candidate',
+//            label: event.candidate.sdpMLineIndex,
+//            id: event.candidate.sdpMid,
+//            candidate: event.candidate.candidate},toSocketId);
+//    } else {
+//        console.log('End of candidates.');
+//    }
+//}
+
+function handleRemoteStreamAdded(event) {
+    console.log('Remote stream added.');
+    remoteVideo.src = window.URL.createObjectURL(event.stream);
+
+    $('.overlay').show();
+    $('.modal').show();
+
+    remoteStream = event.stream;
+}
+
+function handleRemoteStreamRemoved(event) {
+    console.log('Remote stream removed. Event: ', event);
+}
+
+function doCall(toSocketId) {
+    console.log('Sending offer to peer');
+    //peerconnectionList[toSocketId].createOffer(setLocalAndSendMessage(toSocketId), handleCreateOfferError);
+
+
+    peerconnectionList[toSocketId].createOffer(function(sessionDescription){
+        console.log("---------------------"+toSocketId+"------------");
+        console.log("---------------------"+sessionDescription+"------------");
+        peerconnectionList[toSocketId].setLocalDescription(sessionDescription);
+        console.log('setLocalAndSendMessage sending message' , sessionDescription);
+        sendMessage(sessionDescription,toSocketId);
+    }, handleCreateOfferError);
+}
+
+function doAnswer(toSocketId) {
+    console.log('Sending answer to peer.');
+    //peerconnectionList[toSocketId].createAnswer(setLocalAndSendMessage(toSocketId), null, sdpConstraints);
+    peerconnectionList[toSocketId].createAnswer(
+        function(sessionDescription){
+            console.log("---------------------"+toSocketId+"------------");
+            console.log("---------------------"+sessionDescription+"------------");
+            peerconnectionList[toSocketId].setLocalDescription(sessionDescription);
+            console.log('setLocalAndSendMessage sending message' , sessionDescription);
+            sendMessage(sessionDescription,toSocketId);
+        }
+        , null, sdpConstraints);
+}
+
+function setLocalAndSendMessage(sessionDescription,toSocketId) {
+    // Set Opus as the preferred codec in SDP if Opus is present.
+    //sessionDescription.sdp = preferOpus(sessionDescription.sdp);
+    console.log("---------------------"+sessionDescription+"------------");
+    console.log("---------------------"+toSocketId+"------------");
+    peerconnectionList[toSocketId].setLocalDescription(sessionDescription);
+    console.log('setLocalAndSendMessage sending message' , sessionDescription);
+    sendMessage(sessionDescription,toSocketId);
+}
+
+
+
+
+//-----------------extension stream source control---------
+// listen for messages from the content-script
+window.addEventListener('message', function (event) {
+    if (event.origin != window.location.origin) return;
+
+    // content-script will send a 'SS_PING' msg if extension is installed
+    if (event.data.type && (event.data.type === 'SS_PING')) {
+        extensionInstalled = true;
+    }
+
+    // user chose a stream
+    if (event.data.type && (event.data.type === 'SS_DIALOG_SUCCESS')) {
+        //startScreenStreamFrom(event.data.streamId);
+        //displayLocalStream(event.data.streamId);
+        console.log("after emit create or join,the init state is",isInitiator);
+        initSharing(event.data.streamId);
+    }
+
+    // user clicked on 'cancel' in choose media dialog
+    if (event.data.type && (event.data.type === 'SS_DIALOG_CANCEL')) {
+        console.log('User cancelled!');
+    }
+});
+
+
+
+//-----------------Main Logic-------------------
+
+document.getElementById('startSharing').addEventListener('click', function() {
+    // send screen-sharer request to content-script
+    if (!extensionInstalled){
+        var message = 'Please install the extension:\n' +
+            '1. Go to chrome://extensions\n' +
+            '2. Check: "Enable Developer mode"\n' +
+            '3. Click: "Load the unpacked extension..."\n' +
+            '4. Choose "extension" folder from the repository\n' +
+            '5. Reload this page';
+        alert(message);
+    }
+
+    room = document.getElementById("roomName").value;
+
+    console.log('Create or join room', room);
+    socket.emit('create or join', room);
+
+    window.postMessage({ type: 'SS_UI_REQUEST', text: 'start' }, '*');
+});
+
+//----------------------
+document.getElementById('joinSharing').addEventListener('click', function() {
+
+    room = document.getElementById("roomName").value;
+
+    console.log('Create or join room', room);
+    socket.emit('create or join', room);
+
+    //createPeerConnection();
+});
+
+
+function handleUserMedia(stream) {
+    console.log('STEP -- Adding local stream.');
+    localVideo.src = window.URL.createObjectURL(stream);
+
+    $('.overlay').show();
+    $('.modal').show();
+
+    localStream = stream;
+    sendMessage('got user media',room);
+    //if (isInitiator) {
+    //    maybeStart();
+    //}
+}
+
+function handleUserMediaError(error){
+    console.log('getUserMedia error: ', error);
+}
+
+function handleCreateOfferError(event){
+    console.log('createOffer() error: ', event);
+}
+
+function initSharing(inputstreamId){
+//if(isInitiator)
+//{
+    var constraints = {
+        audio: false,
+        video: {
+            mandatory: {
+                chromeMediaSource: 'desktop',
+                chromeMediaSourceId: inputstreamId,
+                maxWidth: window.screen.width,
+                maxHeight: window.screen.height
+            }
+        }
+    };
+    getUserMedia(constraints, handleUserMedia, handleUserMediaError);
+//}
+//else
+//{
+//
+//}
+}
--- server_socketTest.js
+++ server_socketTest.js
@@ -22,6 +22,8 @@ var app = https.createServer(options,function (req, res) {
 
 var io = require('socket.io').listen(app);
 
+var roomOwnerList={}
+
 io.sockets.on('connection', function (socket){
 
     // convenience function to log server messages on the client
@@ -33,13 +35,16 @@ io.sockets.on('connection', function (socket){
         socket.emit('log', array);
     }
 
-    socket.on('message', function (message,roomid) {
+    socket.on('message', function (message,toSocketid) {
         log('Got message:', message);
         // for a real app, would be room only (not broadcast)
         //socket.broadcast.emit('message', message);
 
-        socket.broadcast.to(roomid).emit('message', message);
+        socket.to(toSocketid).emit('message', message,toSocketid,socket.id);
+
+        console.log("Socket---"+socket.id+"---send message to:"+toSocketid+" With MsgType: "+message.type);
     });
+
 
     socket.on('create or join', function (room) {
         //var numClients = io.sockets.clients(room).length;
@@ -53,28 +58,69 @@ io.sockets.on('connection', function (socket){
         //    console.log(test.length);
         //}
 
-
         var numClients = io.sockets.adapter.rooms[room]!=undefined ? io.sockets.adapter.rooms[room].length:0;
         //var numClients = io.sockets.sockets.length;
 
-        console.log(numClients);
+        //console.log('Room ' + room + ' has ' + numClients + ' client(s)');
+        //console.log(io.sockets.adapter.rooms[room]);
 
-        log('Room ' + room + ' has ' + numClients + ' client(s)');
-        log('Request to create or join room ' + room);
+
+        //log('Room ' + room + ' has ' + numClients + ' client(s)');
+        //log('Request to create or join room ' + room);
 
         if (numClients === 0){
             socket.join(room);
             socket.emit('created', room);
-        } else if (numClients === 1) {
-            io.sockets.in(room).emit('join', room);
+
+            console.log("I am init-er");
+
+            roomOwnerList[room]=socket.id;
+
+            console.log('Room ' + room + ' has ' + numClients + ' client(s)');
+            console.log(io.sockets.adapter.rooms[room]);
+            console.log(roomOwnerList);
+
+        } else if (numClients < 5) {
+            io.sockets.in(room).emit('join', room,socket.id);
             socket.join(room);
-            socket.emit('joined', room);
+            socket.emit('joined', room,roomOwnerList[room]);
+
+            console.log("New arraival");
+
+            console.log('Room ' + room + ' has ' + numClients + ' client(s)');
+            console.log(io.sockets.adapter.rooms[room]);
+
         } else { // max two clients
             socket.emit('full', room);
+            console.log("Full");
         }
-        socket.emit('emit(): client ' + socket.id + ' joined room ' + room);
-        socket.broadcast.emit('broadcast(): client ' + socket.id + ' joined room ' + room);
+
+        //socket.emit('emit(): client ' + socket.id + ' joined room ' + room);
+        //socket.broadcast.emit('broadcast(): client ' + socket.id + ' joined room ' + room);
 
     });
 
 });
+
+function initSharing(inputstreamId){
+//if(isInitiator)
+//{
+    var constraints = {
+        audio: false,
+        video: {
+            mandatory: {
+                chromeMediaSource: 'desktop',
+                chromeMediaSourceId: inputstreamId,
+                maxWidth: window.screen.width,
+                maxHeight: window.screen.height
+            }
+        }
+    };
+    getUserMedia(constraints, handleUserMedia, handleUserMediaError);
+//}
+//else
+//{
+//
+//}
+}
+
